de69d281b6d271d207ce5c2d5c6babcebb8f45f5
Dharanidaran <dharanieee@gmail.com>
2016-03-09 13:43:36 +0100

    Adding d3

--- js/customStickyNote.js
+++ js/customStickyNote.js
@@ -1,54 +1,79 @@
-// customStickyNote.js
+// // customStickyNote.js
 
-(function($,window){
+// (function($,window){
 
-	function rand(){
-		return 8;
-	}
+// 	function rand(){
+// 		return 8;
+// 	}
 
-	function StickyNotes( element ){
-		this.el = document.getElementById(element);
-		this.init(); // Only method thats has straight access.
+// 	function StickyNotes( element ){
+// 		this.el = document.getElementById(element);
+// 		this.init(); // Only method thats has straight access.
 	
-	}
+// 	}
 
-	StickyNotes.prototype ={
-		init: function() {
-			this.addNotes = this.el.querySelectorAll(".sticky");
+// 	StickyNotes.prototype ={
+// 		init: function() {
+// 			this.addNotes = this.el.querySelectorAll(".sticky");
+// 			this.selector();
+// 			this.regEvents();
 
-			this.regEvents();
-		},
 
-		regEvents: function() {
+// 		},
 
-			var self = this;
-			console.log(self.addNotes);
-			self.addNotes[0].addEventListener("click", function(){ 
-				// create a dom Element 
+// 		selector: function() {
+// 			var self = this;
+// 			self.notesArray = this.el.querySelectorAll(".note");
+// 		},
 
-					  var note = document.createElement("div");
-					  classie.add( note, 'note' );
-					  var page = document.createElement("div");
-					  classie.add( page, 'page');
 
-					  var h1 = document.createElement("h1");
-					  var content = document.createElement("div");
-					  classie.add( content, 'content');
+// 		draggable: function(element){
+// 			console.log($('.note').draggable());
+// 		},
 
-					  h1.innerHTML = "Heading";
-					  content.innerHTML = " Edit your note here!";
-					  page.appendChild(h1);
-					  page.appendChild(content);
-					  note.appendChild( page);
-					  self.el.appendChild( note);			
 
-			},false);
-		}
-	}
+// 		regEvents: function() {
 
-	var sticky = new StickyNotes("whiteboard")
+// 			var self = this;
+// 			console.log(self.addNotes);
+// 			self.addNotes[0].addEventListener("click", function(){ 
+// 				// dynamically create a Note Element, 
+
+// 					  var note = document.createElement("div");
+// 					  classie.add( note, 'note' );
+// 					  var page = document.createElement("div");
+// 					  classie.add( page, 'page');
+
+// 					  var h1 = document.createElement("h1");
+// 					  var content = document.createElement("div");
+// 					  classie.add( content, 'content');
+
+// 					  h1.innerHTML = "Heading";
+// 					  content.innerHTML = " Edit your note here!";
+// 					  page.appendChild(h1);
+// 					  page.appendChild(content);
+// 					  note.appendChild( page);
+// 					  self.draggable(note);
+// 					  self.el.appendChild( note);	
+// 					  self.selector(); // update selector 	
+// 					  console.log(self.notesArray);	
+
+
+
+// 			},false);
+
+
+console.log(d3);
+
+
+
+
+// 	}
+// 	}
+
+// 	var sticky = new StickyNotes("whiteboard")
 	
 
 
-})(jQuery,window);
+// })(jQuery,window);
 
