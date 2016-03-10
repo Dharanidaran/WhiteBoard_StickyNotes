8b91de6691215bf5a37ea1b793ed4539a36eece1
Dharanidaran <dharanieee@gmail.com>
2016-03-10 15:44:33 +0100

    Sticky Board application using JQueryUI

---
+++ js/customStickyNotes.js
@@ -0,0 +1,189 @@
+// customStickyNotes.js
+( function($,window){
+
+
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
+
+
+
+
+
+	counter=1000;
+	// Create a function Object
+	function StickyNote(element){
+
+		if (element == undefined){
+			return
+		}
+		this.el = document.querySelector (element);
+		this.addNote = document.getElementById("addNote");
+		this.init();
+
+		
+	}
+
+	StickyNote.prototype ={
+		init: function(){
+			this.regEvents();
+		},
+
+		regEvents : function(){
+			var self = this;
+
+			self.addNote.addEventListener( "click",this._createNote,false);
+
+			self.el.addEventListener("click",function(event){
+				event.stopPropagation(); // Not really needed for now
+				
+				$(".stickynote").removeClass('active')
+				var array=$(".stickynote");
+				
+			},false);
+
+			// usage:
+			$(window).smartresize(function(){
+			  // code that takes it easy...
+			  console.log(this);
+			});
+
+
+		},
+
+		_createNote : function(e){
+
+			e.stopPropagation();
+			var self = this;
+			counter++;
+			//Create unique id here
+			id ="element_"+counter;
+			idControl = "control_"+counter;
+			//Function to create a single note
+			var div = document.createElement("div");
+				div.setAttribute("id",id);
+				//div.setAttribute("contenteditable", "true");
+			var contentText = document.createTextNode("Click to edit");
+				div.appendChild(contentText);
+				classie.add(div,"stickynote");
+			var control = document.createElement("div");
+				control.setAttribute("id",idControl);
+				control.setAttribute("class","control");
+				div.appendChild(control);
+			
+			var cDelete = document.createElement('div');
+				control.appendChild(cDelete);
+				control.setAttribute("contenteditable", "false");
+
+				classie.add(cDelete,"controlElement");
+				classie.add(cDelete,"delete");
+
+			var colorRose = document.createElement('div');
+				control.appendChild(colorRose);
+				colorRose.setAttribute("contenteditable", "false");
+				colorRose.style.backgroundColor = "#FFA3CE";
+
+
+				classie.add(colorRose,"controlElement");
+				classie.add(colorRose,"colorElement");
+
+
+			var colorOrange = document.createElement('div');
+				control.appendChild(colorOrange);
+				colorOrange.setAttribute("contenteditable", "false");
+				colorOrange.style.backgroundColor = "#FF7767";
+
+				classie.add(colorOrange,"controlElement");	
+				classie.add(colorOrange,"colorElement");
+
+			var colorGreen = document.createElement('div');
+				control.appendChild(colorGreen);
+				colorGreen.setAttribute("contenteditable", "false");
+				colorGreen.style.backgroundColor = "#41AB65";
+
+				classie.add(colorGreen,"colorElement");
+				classie.add(colorGreen,"controlElement");
+
+
+			// add it to the  whiteboard
+
+			// This is attached to addNote element 
+			//so get a handle to the whiteboard
+				var whiteboard = this.parentNode;
+				whiteboard.appendChild(div);
+
+				// Bind the event after attaching
+				// $(id).addClass('active');
+
+				$(div).draggable({ 					
+						distance: 10,
+						cursorAt: { left: 0, top:0 },
+						cursor: "crosshair",
+						containment: [ 75,75,window.innerWidth-75, window.innerHeight-75],
+					})
+					.click(function(e) {
+						e.stopPropagation();
+						$(this).draggable( { disabled: false });
+						$(this).addClass("active");
+						$(this).attr("contenteditable", "true");
+
+
+					});
+					//.dblclick(function(e) {
+						//e.stopPropagation();
+						//$(this).draggable({ disabled: true });
+						//$(this).addClass("Editing");
+					//});
+				
+					$(".delete").click( function(e){
+						e.stopPropagation();
+						$(this).closest('.stickynote').remove();
+					})
+
+					$(".colorElement").click(function(e){
+						e.stopPropagation();
+						var colorClicked =$(this).css("backgroundColor");
+						console.log(colorClicked);
+
+						$(this).closest('.stickynote').css('background-color', colorClicked);
+					})
+
+
+		
+		},
+
+		_bindNoteEvent : function(){
+			// Function to attach the neccessary events to a single note
+			console.log('Binding EVENTS ....');
+
+			//attach drag event
+
+		}
+	}
+	var stickynote = new StickyNote("#whiteboard");
+
+})(jQuery,window);
--- js/stickyNotes.js
+++ js/stickyNotes.js
@@ -1,1 +1,121 @@
-stickyNotes.js
+
+var width = 300,
+	height =200;
+var data = [1,2,3,4];
+var inside
+
+var clickCount = 1000;
+asdfas
+
+function click(){
+	//if (d3.event.defaultPrevented ) return;
+
+	// Extract the click Location 
+	clickCount++;
+
+	var point = d3.mouse( this ),
+		p ={ x: point[0], y: point[1] };
+
+		id ="dragMe_"+clickCount;
+
+	var note= section.append("div")
+		.attr("id",id)
+		.style("top","200px")
+		.style("left","200px")
+		.attr("width",200)
+		.attr("height",200)
+		.attr("class", "note");
+			var page = note.append("div")
+			.attr("class","page")
+			.attr("contenteditable","true");
+		bindMe(id);
+		
+
+}
+var section = d3.select("#whiteboard");
+var sticky = d3.select("#sticky").on("click", click);
+
+
+// Define drag beavior
+var drag = d3.behavior.drag()
+    .on("drag", dragmove);
+
+function dragmove(d) {
+  var x = d3.event.x;
+  var y = d3.event.y;
+
+  // Check for coditions here!
+  d3.select(this).style("top", y+"px").style("left", x+"px");
+  // attr("transform", "translate(" + x + "," + y + ")");
+}
+
+function handle(){
+	console.log('clicked');
+	
+}
+
+function bindMe(id){
+
+		console.log(" asdkjfa");
+		var selector ="#"+id;
+		$(selector).draggable({
+				distance: 10,
+				cursorAt: { left: 0, top:0 },
+				cursor: "crosshair",
+				containment: [ 50, 50, 900, 600 ],
+			})
+			.click(function() {
+			$(this).draggable( {disabled: false});
+			})//.dblclick(function() {
+			//$(this).draggable({ disabled: true });
+
+		});
+}
+
+
+
+
+// <!doctype html>
+// <html lang="en">
+// <head>
+//   <meta charset="utf-8">
+//   <title>jQuery UI Draggable - Default functionality</title>
+//   <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+//   <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+//   <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+//   <link rel="stylesheet" href="style.css">
+//   <style>
+//   #draggable { width: 150px; height: 150px; padding: 0.5em; }
+//   </style>
+
+// </head>
+// <body>
+ 
+// <div contenteditable="true" id="d" class='d'>
+
+// <span>Text to edit</span>
+// </div>
+
+// <div contenteditable="true" id="d" class='d'>
+
+// <span>Text to edit</span>
+// </div>
+// <div contenteditable="true" id="d" class='d'>
+
+// <span>Text to edit</span>
+// </div>
+
+// <div id="draggable" class="ui-widget-content">
+//   <p>Drag me around</p>
+// </div>
+//    <script>
+// $(".d").draggable()
+//   .click(function() {
+//     $(this).draggable( {disabled: false});
+// }).dblclick(function() {
+//     $(this).draggable({ disabled: true });
+// });
+//   </script>
+ 
+// </body>
+// </html>
